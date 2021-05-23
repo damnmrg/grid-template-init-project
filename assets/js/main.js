ab3a16e4c197aeefe7ca1a502414214c1ae3890e
damnmrg <nemohaymargarita@gmail.com>
2021-05-23 14:22:29 +0300

    create section about

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,6 @@ $(document).ready(function(){
    $('.header-navbar__list').stop(true, true).fadeToggle(500);
   });
  });
+ window.matchMedia('(min-width: 768px)').addListener(function() {
+  $('.header-navbar__list').css('display', '');
+});
